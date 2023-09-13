441dfaf543800e6159e39f99678268282f682cdb
ThaiToni <vietthaist01@gmail.com>
2023-09-13 16:37:12 +0700

    responsive mobile

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -11,44 +11,47 @@ function Body() {
   }, []);
 
   return (
-    <main className="flex flex-col items-center bg-[#F6F8FC]">
+    <main className="flex flex-col items-center bg-[#F6F8FC] ">
       {data.map((post) => (
         <div
           key={post.id}
-          className="w-[680px] h-fit rounded-[8px] shadow-md mt-[8px] flex flex-col items-center pt-[8px] pb-[8px]">
-          <div className=" flex items-center pt-[8px] pb-[8px] pl-[16px] pr-[16px]">
-            <div className="flex w-[608px] h-[40px]">
+          className="sm:max-w-[680px] h-fit rounded-[8px] shadow-md sm:mt-[16px] flex flex-col pt-[8px] pb-[8px] p-[16px] max-content">
+          <div className=" flex items-center justify-between pt-[8px] pb-[8px] space-x-2 ">
+            <div className="flex  h-[40px]  max-sm:h-[52px]">
               <img src="Avatar.png" className="w-[40px] h-[40px]" alt="" />
-              <div className="ml-[8px]">
-                <p className="font-semibold">{post.name}</p>
-                <p className="text-[#94A3B8] text-[14px]">3 minutes ago</p>
+              <div className="ml-[8px] flex sm:flex-wrap max-sm:flex-col max-sm:text-[14px] leading-none max-sm:justify-around">
+                <p className="font-semibold order-1 ">{post.name}</p>
+                <p className="text-[#94A3B8] text-[14px] w-[100%] order-2 max-sm:order-3">
+                  3 minutes ago
+                </p>
+                <p className="text-[#497CFF] sm:ml-[10px] order-1 max-sm:order-2">
+                  @{post.userName}
+                </p>
               </div>
-              <p className="text-[#497CFF] ml-[10px]">@{post.userName}</p>
             </div>
-            <img
-              src={post.socials}
-              className="w-[32px] h-[32px] ml-[8px]"
-              alt=""
-            />
+            <img src={post.socials} alt="" />
           </div>
-          <p className="w-full pt-[8px] pl-[16px] pr-[16px] pb-[16px] ">
-            {post.title}
-          </p>
-          <div className="flex space-x-[8px] pl-[16px] pr-[16px]">
+          <p className="w-full pt-[8px] pb-[16px] ">{post.title}</p>
+          <div
+            className={`grid gap-2 ${
+              post?.image?.length > 1 ? "grid-cols-2" : "grid-cols-1"
+            }`}>
             {post?.image?.map(
               (image) =>
                 image && (
                   <img
                     src={image}
-                    className="w-full h-[369px] rounded-[8px]"
+                    className="h-auto rounded-[8px] "
                     alt=""
                   />
                 )
             )}
           </div>
-          <div className="w-full pl-[16px] pr-[16px] pb-[16px] pt-[8px] mt-[8px] leading-[140%]">
-              <p className="text-[#0072F8] inline">{post.caption.hastag}</p>
-              <p className="inline ml-[8px]">{post.caption.content}</p>
+          <div className="w-full  pb-[16px] pt-[8px] mt-[8px] leading-[140%]">
+            <p className="text-[#0072F8] inline mr-[8px]">
+              {post.caption.hastag}
+            </p>
+            <p className="inline ">{post.caption.content}</p>
           </div>
         </div>
       ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,17 @@
 import React from "react";
+import "./header.css";
 
 function Header() {
   return (
-    <main className="h-[100px] flex shadow-md relative">
-      <img
-        src="Frame.png"
-        className="w-[62px] h-[60px] absolute ml-[38px] mt-[20px]"
-        alt=""
-      />
+    <main className="sm:h-[100px] h-[60px] flex shadow-md relative">
+      <img src="Frame.png" className=" absolute sm:ml-[38px] sm:mt-[20px] w-[41.357px] h-[40px] ml-[16px] mt-[9px]" alt="" />
       <img
         src="Union.png"
-        className="w-[189.996px] h-[53.446px] absolute ml-[110.35px] mt-[22.96px]"
+        className="sm:w-[189.996px] sm:h-[53.446px] w-[126.737px] h-[36px] absolute sm:ml-[110.35px] sm:mt-[22.96px] ml-[64.26px] mt-[10.97px]"
         alt=""
       />
 
-      <p className="flex justify-center absolute w-full items-center h-full text-[24px] uppercase leading-[140%] font-bold text-[#005A92]">
+      <p className="flex headertitle justify-center absolute w-full items-center h-full text-[24px] uppercase leading-[140%] font-bold text-[#005A92] max-sm:hidden">
         9th Global Conference of Young Parliamentarians
       </p>
     </main>
